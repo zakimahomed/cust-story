4dd8b0b42e591aae77e28a9cc817e6b55085b42b
Zaki Mahomed <alizaki@gmail.com>
2019-09-23 15:14:57 -0700

    init

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    MediaRecorder.start();
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
 };
