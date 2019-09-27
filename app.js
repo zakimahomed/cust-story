7647150a58a6f76e4ae6faade8970db2c1ca61c0
Seivan Heidari <seivan.heidari@icloud.com>
2019-09-27 20:47:27 +0200

    wip

--- app.js
+++ app.js
@@ -66,13 +66,54 @@ cameraTrigger.onclick = function () {
         let playback = document.querySelector("#playback")
         playback.pause()
 
+        let playbackInterval = setInterval(function () {
+            // console.log("interval")
+            // console.log()
+
+            var img = new Image
+
+            img.onload = function () {
+                var ctx = cameraSensor.getContext("2d")
+                ctx.drawImage(img, 0, 0); // Or at whatever offset you like
+            };
+            let currentTimeStamp = playback.currentTime * 1000
+
+            var foundImage = null
+            let i = 0;
+            do {
+                let image = images[i]
+                if (currentTimeStamp >= image.startTimeStamp && currentTimeStamp <= image.endTimeStamp) {
+                    foundImage = image
+                }
+                i++;
+            }
+            while (foundImage == null && i < images.length)
+            if (foundImage) {
+                img.src = foundImage.image;
+            }
+            // cameraSensor.getContext("2d").drawImage(images[0].image, 0, 0);
+
+
+
+        }, 1000 / fps)
         // playback.onclick = function (e) {
         console.log("CLICKED")
         playback.src = videoURL
         playback.load()
         playback.play()
+
+        playback.onended = (e) => {
+            clearInterval(playbackInterval)
+        }
+        playback.ontimeupdate = (event) => {
+            // console.log("ontimeupdate")
+            // console.log(event.target.currentTime)
+            //   console.log('The currentTime attribute has been updated. Again.');
+        };
+
         // }
         // videoURL = null
+        // isRecording = false
         cameraTrigger.textContent = "Playing... NEW RECORDING"
 
     }
@@ -87,98 +128,25 @@ cameraTrigger.onclick = function () {
         }
 
         isRecording = false
-        var frames = []
+        // var frames = []
 
-        for (let i = 0; i < images.length; i++) {
-            frames.push({ data: images[i], name: 'input_' + String(i) + '.jpeg' })
-        }
-        images = []
+        // for (let i = 0; i < images.length; i++) {
+        //     frames.push({ data: images[i], name: 'input_' + String(i) + '.jpeg' })
+        // }
+        // console.log(images)
+        // images = []
 
 
 
         rec.exportWAV(function (audioBlob) {
-            // let audioURL = URL.createObjectURL(audioBlob);
-            // console.log("AUDIO URL")
-            // console.log(audioBlob)
-            // console.log(audioURL)
-            // '-s', '1920x1080',
-            // let imageToVideo = ffmpeg_run({
-            //     arguments: [
-            //         '-r', '24',
-            //         '-i', 'input_%d.jpeg',
-            //         '-v', 'verbose',
-            //         '-nostdin',
+            let audioURL = URL.createObjectURL(audioBlob);
 
-            //         'output.mp4'
-            //     ],
-            //     files: frames,
-            //     type: "command",
-
-            //     TOTAL_MEMORY: 268435456
-            // });
-            // console.log(imageToVideo)
-
-            var reader = new FileReader()
-            reader.readAsArrayBuffer(audioBlob)
-            reader.onloadend = (event) => {
-                // console.log(event)
-                // console.log(reader)
-                // console.log(reader.result)
-                // var audioBlob = new Blob([reader.result], { type: 'audio/wav' });
-                // let url = URL.createObjectURL(audioBlob);
-                // var link = document.createElement('a');
-
-
-                // link.href = url;
-                // link.download = new Date().toISOString() + '.wav';
-                // link.innerHTML = link.download;
-
-                // document.body.appendChild(link);
-                // link.click();
-                // console.log(imageToVideo[0].data)
-                // let attachments = [
-                //     { data: imageToVideo[0].data, name: 'video_sans_audio.mp4' },
-
-                frames.push({ data: new Uint8Array(reader.result), name: 'audio.wav' })
-                // ]
-                // '-map', '0:0',
-                // '-map', '1:0',
-                // '-c:v', 'copy',
-                // '-c:a', 'copy',
-
-
-                let combineAudioAndVideo = ffmpeg_run({
-                    arguments: [
-                        '-r', `${fps}`,
-                        '-i', 'input_%d.jpeg',
-                        '-i', 'audio.wav',
-                        '-v', 'verbose',
-                        '-vf', 'hflip',
-                        '-nostdin',
-                        '-strict', '-2',
-                        'output.mp4'
-                    ],
-                    files: frames,
-                    type: "command",
-
-                    TOTAL_MEMORY: 268435456
-                })
-                // console.log(combineAudioAndVideo)
-                if (combineAudioAndVideo[0] == null) {
-                    alert("ERROR")
-                }
-                console.log(combineAudioAndVideo[0].data)
-                videoBlob = new Blob([combineAudioAndVideo[0].data], { type: 'video/mp4' });
-                console.log(videoBlob)
-                videoURL = URL.createObjectURL(videoBlob);
-
-                cameraView.style.visibility = "hidden";
-                cameraTrigger.textContent = "PLAY RECORDING"
+            videoURL = audioURL
+            cameraView.style.visibility = "hidden";
+            cameraTrigger.textContent = "PLAY RECORDING"
 
 
 
-
-            }
 
 
 
@@ -193,11 +161,26 @@ cameraTrigger.onclick = function () {
         isRecording = true
         cameraTrigger.textContent = "STOP RECORDING"
 
-
+        let startDate = Date.now()
+        var previousTimeStamp = null
         intervalId = setInterval(function () {
             cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
             let x = cameraSensor.toDataURL("image/jpeg");
-            images.push(convertDataURIToBinary(x))
+
+            let endTimeStamp = Date.now() - startDate
+
+            var startTimeStamp = previousTimeStamp
+
+            if (startTimeStamp == null) {
+                startTimeStamp = 0
+            }
+
+            previousTimeStamp = endTimeStamp
+
+            images.push({ image: x, startTimeStamp: startTimeStamp, endTimeStamp: endTimeStamp })
+
+
+
 
         }, 1000 / fps)
 
